ccd66100f0663ac330c3428bd72e431e3607f4cc
SiYuanLiuCrazy <siyuanliu_csu@163.com>
2024-07-22 14:05:42 +0800

    自动命名 自动打标签

--- ppt_lable/myapp/views.py
+++ ppt_lable/myapp/views.py
@@ -11,7 +11,13 @@ import shutil
 from django.core.files.storage import FileSystemStorage
 from spire.presentation import *
 License.SetLicenseKey("3vgBAP6gsMpt19Lp5/dLFp/xNXCyz2vrwWXv6RZ9dhyEE6o0AFXtRmuqeHo1LxSwxYqobFgqbUkY/8y0oqHns0AgcGdhUi/gxW3rgxU03m9Je53kYu9SVmpp/9SEEkUhzJ08Ir3+/014o07W0UXzj63zhaQjMxz5Lmx+U08qabOyQmGmnbolIYd70slekKohJLucSw9KL/OxAzIhBdI1+IaElvL2jUsl1w0zGfyVVG56vcENaCh23rDBNUdkTHccDEOW9L3Qa1DZtUpJadmED0scQbbsDHtrJ8sR03yxa2stkpjXUYcVyE9xZphrvWHYvvxAt1UOFOpYxzSwvxS7TIFEhXQBzcHRpEidD1BnBQgIMvo9tdqQQCKCx4k6r9b0++Xs0vwJGUyJGg5a2hVtYnTcPp4ZfPZP8U1XI3sEaVr7BKy44sN4aFRUcOg8E5BVsXJdZQZnWxm0ZPFRazyGL0Ki6Vi0FchpZ1zlqjR15y8GrdZqd2Y4TqYp6gotvPdSH9uirPRDJTPvAvemo491EoPQo83odCDE2MoYu6dm2zNJWc0/ac6sPCt2WR5p9gibTkyjpzW9t+d8YVbjszXSecM+gi5WIBVg98flw/RVhsNg6kxqQ9EqyZXvYxXBCPoygWjJRlC8ETyeMkQy9Cw3t1nUCeFjrpJlWQMuavMa79tLT0anjYYbbl9k/j73i5lG4tmSpCFqgC+WGIzCHDKm5+uLU8MwAzwlFOZ11b2q/rR8Mf/eYczGtKwgvq8HBPx+QFbc3imAYQVhx1Jf2nvcMSD+2x17L+PBoRhvaTRmgcS3+iiB2FBI99OT3qbuIIGsG5X168kbFQH3spxYHUa2Yuik2izLDk+Mki1abBRAyC0cK47EDEtRSc8qp6BbHu4wd2YFYuZ7kYkVcoeB2aSzpKkzVQ3cNy5RU0ovTVxz4asAHUJ0kAzvX17MVsMjQ99rAN0q3WIJufuXAiY8UcSvInRxGIcnPiueETdldin8Cx5ww1yORCHRd41tYEHsGld2nKd2TaE5mGmHiYzFKKqnPFnR8ckY/B7VDslP5HdUkX6V7/aoyKMzh6X99J0PDU51A0dq0aWn6O7J2bBAShj+rjqPT1hljqGoQ37BDDhwD5ab/0Ps2JrrteOiCJbS/KyAFbZ9p6UF7SQZTFzLqaHqZVn0Qz1vgj7PhlSgvBPfGblX5GLUQzAvE8bhh9Xm2RUFTwbwem9rMXJT5hT59GdNXCPmHPpDnSBs0JCi4fB0LVLQgvpnUewkhVVgPA3v6YWBK3JdU3kb6no561XwJ5u0H+TEgXS3hL1qxnnDKlnMVgbf+DG4P0GU0ManBNNM6deXmUks5/DgO4xM2W5EWbCO0+qmGre+c9c+WBt8eflMI+HPjSIcdWeyUauO76+6tesHzIEwTTGYAkMB4KA581Ct5LTYuzv2SA2PS16VflU8mlcn2mya0sDBWwQWmyxct73Dn8NQk9OcZuk9hbBDGjIEl8wHZ161zxhexR4fU51/yDtaGx+E6usezdPVceX8GRNXoLPWWjyCcUeJ19Zax6eL8/nMr70vL03u3nNGeiBFOL4rg5EHSENVFARTAJU2gYZFo6WauXd9N711W2WST5JXOA==")
-from spire.presentation.common import *
+from spire.presentation.common import * 
+from pathlib import Path
+from openai import OpenAI
+import tempfile
+import time
+from openai import RateLimitError, AuthenticationError
+import json
 
 def create_folder_view(request):
     if request.method == 'POST':
@@ -345,6 +351,7 @@ def edit_ppt_view(request):
 
         return redirect('catalog_detail', catalog_id=ppt.catalog)
 
+
 def split_ppt_view(request):
     if request.method == 'POST':
         ppt_id = request.POST.get('ppt_id')
@@ -368,25 +375,78 @@ def split_ppt_view(request):
             parent_id=parent_catalog.id
         )
 
-        for i in range(slide_count):
-            new_presentation = Presentation()
-            new_slide = presentation.Slides[i]
+        client = OpenAI(
+            api_key = "sk-qwEg1ba3qj8JNsUtErVmlDefkK9uRbW60MyEPHeO1d4Lt67y",
+            base_url = "https://api.moonshot.cn/v1",
+        )
 
+        for i in range(slide_count):
             slide_filename = f"{output_folder_name}_slide{i + 1}.pptx"
             output_file_path = os.path.join(output_folder_path, slide_filename)
-            new_slide.SaveToFile(output_file_path, FileFormat.Pptx2019)
+            
+            # 创建单页的PPT文件
+            new_presentation = Presentation()
+            new_slide = presentation.Slides[i]
+           
 
+            # 保存为临时PDF文件
+            temp_pdf_fd, temp_pdf_path = tempfile.mkstemp(suffix=".pdf")
+            os.close(temp_pdf_fd)
+            new_slide.SaveToFile(temp_pdf_path, FileFormat.PDF)
+
+            # 使用临时PDF文件内容生成标题
+            try:
+                title, lable = generate_title_from_pdf(client, temp_pdf_path)
+            except AuthenticationError:
+                return HttpResponse('Invalid Authentication', status=401)
+            except RateLimitError:
+                return HttpResponse('Rate limit reached. Please try again later.', status=429)
+
+            # 保存当前幻灯片到文件
+            new_slide.SaveToFile(output_file_path, FileFormat.Pptx2019)
+            new_presentation.Dispose()
+
+            # 保存拆分出来的PPT文件信息到数据库
             TyPptMain.objects.create(
-                title=f"{ppt.title}_slide{i + 1}",
-                lable=ppt.lable,
+                title=title,
+                lable=lable,
                 catalog=catalog.id,
                 name=slide_filename,
                 type="pptx",
                 size=os.path.getsize(output_file_path),
                 path=output_file_path
             )
-            new_presentation.Dispose()
 
+            # 删除临时PDF文件
+            os.remove(temp_pdf_path)
+            time.sleep(30)
         presentation.Dispose()
 
         return redirect('catalog_detail', catalog_id=ppt.catalog)
+    
+def generate_title_from_pdf(client, temp_pdf_path):
+    file_object = client.files.create(file=Path(temp_pdf_path), purpose="file-extract")
+    file_content = client.files.content(file_id=file_object.id).text
+    messages = [
+        {
+            "role": "system",
+            "content": "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，不可翻译成其他语言。",
+        },
+        {"role": "system", "content": file_content},
+        {"role": "user", "content": "根据我上传的文件起一个10个字以内的名字，同时给5个描述内容的标签，注意要精简、准确，请不要自己过度解读，不要添加文件中不存在的内容。要求你回复内容，他们之间用逗号隔开，不要换行，不要有其它内容，请注意，一个名字，五个标签,数量要正确,返回Json格式"},
+    ]
+
+    retries = 3
+    for i in range(retries):
+        try:
+            completion = client.chat.completions.create(model="moonshot-v1-32k", messages=messages, temperature=0.3)
+            title_and_lable = completion.choices[0].message.content.strip()
+            parsed_data = json.loads(title_and_lable)  
+            title = parsed_data["name"]
+            lables = parsed_data["tags"]
+            return title,lables
+        except RateLimitError:
+            if i < retries - 1:
+                time.sleep(2 ** (i + 1))
+            else:
+                raise
